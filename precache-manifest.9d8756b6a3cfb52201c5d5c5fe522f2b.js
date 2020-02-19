68a0291930a0abe5b11853359736ccde02255674
howdy-mj <hi.minjungkim@gmail.com>
2020-02-20 01:40:02 +0900

    Updates

---
+++ precache-manifest.9d8756b6a3cfb52201c5d5c5fe522f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad96c752c1b8aaa49b3e63b0545a225d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "9ee22d5a62089cebfbf2",
+    "url": "/movie_app_2020/static/css/main.7c656c85.chunk.css"
+  },
+  {
+    "revision": "39b8bcc7d2d72670fac8",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee22d5a62089cebfbf2",
+    "url": "/movie_app_2020/static/js/main.f9af9079.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
