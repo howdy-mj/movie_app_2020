36fdb6033603ccf10784c5b0214fe5d43cf7fd21
howdy-mj <hi.minjungkim@gmail.com>
2020-02-20 01:13:37 +0900

    4.4-5 CSS & Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 import "./Movie.css"
 
 function Movie({ year, title, summary, poster, genres}) {
@@ -8,12 +8,12 @@ function Movie({ year, title, summary, poster, genres}) {
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
             <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
+            <ul className="movie__genres">
                 {genres.map( (genre, index) => (
                     <li key={index} className="genres__genre">{genre}</li>
                 ))}
             </ul>
-            <p className="movie__summary">{summary}</p>
+            <p className="movie__summary">{summary.slice(0, 200)}...</p>
         </div>
     </div>
 }
